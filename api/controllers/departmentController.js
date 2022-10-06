032dcf6079d7b3f715b754db893039c03c01ebab
callistus406 <callistus406@gmail.com>
2022-10-06 16:47:26 +0100

    modified department controller

--- api/controllers/departmentController.js
+++ api/controllers/departmentController.js
@@ -1,8 +1,11 @@
-const DepartmentModel = require("../../models/departmentModel.js");
-const { createDeptValidation } = require("../../validation/validation");
+const DepartmentModel = require('../../models/departmentModel.js');
+const {
+  createDeptValidation,
+  updateDeptValidation,
+} = require('../../validation/validation');
 const createDepartment = async (req, res) => {
-  const { dept_name, head_agent, email, members } = req.body;
   try {
+    const { dept_name, head_agent, email, members } = req.body;
     if (req.user.id && req.user.role === 3) {
       const validateData = { dept_name, head_agent, email };
       const { error } = await createDeptValidation(validateData);
@@ -16,7 +19,7 @@ const createDepartment = async (req, res) => {
         if (getDept) {
           res.status(409).json({
             success: false,
-            message: "department already created",
+            message: 'department already created',
           });
         } else {
           const newDepartment = new DepartmentModel({
@@ -29,7 +32,7 @@ const createDepartment = async (req, res) => {
           // if (savedDept.acknowledged) {
           res.status(200).json({
             success: true,
-            message: "department created",
+            message: 'department created',
             obj: savedDept,
           });
           // }
@@ -38,7 +41,7 @@ const createDepartment = async (req, res) => {
     } else {
       res.status(400).json({
         success: false,
-        message: "you are not authorized to perform this operation",
+        message: 'you are not authorized to perform this operation',
       });
     }
   } catch (error) {
@@ -46,86 +49,162 @@ const createDepartment = async (req, res) => {
   }
 };
 
-// get departments
+// get all departments
 const getDepartment = async (req, res) => {
   try {
+    // verifies if the user is authenticated
     if (req.user.id && req.user.role === 3) {
       const getAllDept = await DepartmentModel.find();
+
       if (!getAllDept)
         return res
-          .status(400)
-          .json({ success: false, result: "No department found" });
+          .status(404)
+          .json({ success: false, result: 'No department found' });
+      // sends response to frontend
       res.status(200).json({ success: true, result: getAllDept });
     } else {
-      res.status(400).json({ success: false, result: "something went wrong" });
+      res.status(403).json({
+        success: false,
+        result: 'you are not authorized to access this resource',
+      });
     }
   } catch (error) {
     res.status(500).json(error.message);
   }
 };
 
-// get department
+// get a  department with its id
+
 const viewDepartment = async (req, res) => {
   try {
-    if (req.user.id && req.user.role === 3 && req.params.deptId) {
-      const getDept = await DepartmentModel.findOne({ _id: req.params.deptId });
+    // verifies if the user is authenticated
+    if (!req.user.id && !req.user.role === 3)
+      return res
+        .status(401)
+        .json({ success: false, result: 'You are not authenticated' });
+    // checks if department id is provided{not functional yet}
+    if (!req.params)
+      return res
+        .status(400)
+        .json({ success: false, result: 'Select a valid  department' });
 
-      if (!getDept)
-        return res.status(400).json({ success: false, result: "not found" });
-      res.status(200).json({
-        success: true,
-        result: getDept,
-      });
-    } else {
-      res.status(400).json({
-        success: false,
-        result: "something went wrong",
-      });
-    }
+    const getDept = await DepartmentModel.findOne({ _id: req.params.deptId });
+    // cj
+    if (!getDept)
+      return res
+        .status(404)
+        .json({ success: false, result: 'department not found' });
+    res.status(200).json({
+      success: true,
+      result: getDept,
+    });
   } catch (error) {
     res.status(500).json(error.message);
   }
 };
-//find all depts
-// get params
-// find de
 
-// get departments
+// updates a specified  department
+
+// validate if no was returned
+// create functionality for removing agents from department
+
 const updateDepartment = async (req, res) => {
   const { dept_name, head_agent, email, members } = req.body;
 
   try {
-    if (req.user.id && req.user.role === 3 && req.params.deptId) {
-      //still working on this
-      const validateData = { dept_name, head_agent, email };
-      const { error } = await createDeptValidation(validateData);
-      if (error) return res.status(400).json(error.message);
-      if (!error) {
-        // checks if user is already in the department
-        const getDept = await DepartmentModel.findOne({ members: members });
-        if (getDept)
-          return res.status(409).json("user already exist in department");
-        // executes if no result was returned from the first search
-        const updatedDept = await DepartmentModel.updateOne(
+    if (!req.user.id && !req.user.role === 3)
+      return res.status(401).json({
+        success: false,
+        result: 'You are not authorized to access this resource',
+      });
+
+    if (!req.params.deptId)
+      return res
+        .status(400)
+        .json({ success: false, result: 'Please select a department' });
+    //still working on this
+    const validateData = { dept_name, head_agent, email };
+    const { error } = await updateDeptValidation(validateData);
+    if (error) return res.status(400).json(error.message);
+    if (!error) {
+      // checks if user is already in the department
+      const getDept = await DepartmentModel.findOne({ members: members });
+      // checks if already exists in a department
+      if (getDept)
+        return res.status(409).json('user already exist in department');
+      // executes if no result was returned from the first search
+      const updatedDept = await DepartmentModel.updateOne(
+        {
+          _id: req.params.deptId,
+        },
+        {
+          dept_name,
+          head_agent,
+          email,
+          $push: { members: members },
+        }
+      );
+
+      res.status(200).json({
+        success: true,
+        result: updatedDept,
+      });
+    }
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+const removeAgentFromDepartment = async (req, res) => {
+  const { dept_name, head_agent, email, userId } = req.body;
+  console.log(userId);
+  try {
+    if (!req.user)
+      return res.status(401).json({
+        success: false,
+        result: 'You are not authenticated',
+      });
+    if (!req.user.id && !req.user.role === 3)
+      return res.status(401).json({
+        success: false,
+        result: 'You are not authorized to access this resource',
+      });
+
+    if (!req.params.deptId)
+      return res
+        .status(400)
+        .json({ success: false, result: 'Please select a department' });
+    const validateData = { dept_name, head_agent, email };
+    const { error } = await updateDeptValidation(validateData);
+    if (error) return res.status(400).json(error.message);
+
+    if (!error) {
+      // checks if user is already in the department
+      const getDept = await DepartmentModel.findOne({
+        $and: [{ _id: req.params.deptId }, { members: userId }],
+      });
+      console.log(getDept);
+      if (getDept) {
+        const deleteAgent = await DepartmentModel.updateOne(
           {
             _id: req.params.deptId,
           },
           {
-            dept_name,
-            head_agent,
-            email,
-            $push: { members: members },
+            $pull: { members: userId },
           }
         );
-
         res.status(200).json({
           success: true,
-          result: updatedDept,
+          result: deleteAgent,
         });
       }
+      // return res.status(409).json('user already exist in department');
     }
   } catch (error) {
-    res.status(500).json(error.message);
+    res.status(500).json({
+      success: false,
+      result: error.message,
+    });
   }
 };
 
@@ -144,7 +223,7 @@ const deleteDepartment = async (req, res) => {
     } else {
       res.status(400).json({
         success: false,
-        message: "You are not authorized to perform this operation",
+        message: 'You are not authorized to perform this operation',
       });
     }
     // res.status(200).json("delete department route");
@@ -159,4 +238,5 @@ module.exports = {
   deleteDepartment,
   updateDepartment,
   viewDepartment,
+  removeAgentFromDepartment,
 };
