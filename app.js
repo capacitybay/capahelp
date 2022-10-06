447c631314a159dee61c24f281c04aa222c29182
callistus406 <callistus406@gmail.com>
2022-10-06 16:46:24 +0100

    added custom error handler middleware

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-const express = require("express");
-require("dotenv").config();
-const routes = require("./api");
+const express = require('express');
+require('dotenv').config();
+const routes = require('./api');
 const app = express();
-const connection = require("./db/connection");
+const errorHandler = require('./middleware/errorHandler');
+const error404 = require('./middleware/error404');
+const connectDb = require('./db/connect');
 
 // application port
 const port = process.env.PORT || 4000;
 // use middleWares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/api/v1", routes);
-connection();
-
-// connect to database
-app.listen(port, () => {
-  console.log(`app is listening on port ${port}`);
-});
+app.use('/api/v1', routes);
+app.use(error404);
+app.use(errorHandler);
+const startApp = async () => {
+  try {
+    // connect to database
+    await connectDb(process.env.MONGO_URL);
+    console.log(`mongodb is connected`);
+    app.listen(port, () => {
+      console.log(`app is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+// starts application
+startApp();
