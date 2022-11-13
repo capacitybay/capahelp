71c60b2d379ff3f2e2aa6bcfdfbb3fce2e8343cd
callistus406 <callistus406@gmail.com>
2022-11-13 23:01:15 +0100

    testing

--- models/userModel.js
+++ models/userModel.js
@@ -47,7 +47,7 @@ const UserSchema = new mongoose.Schema(
     },
     active: {
       type: Boolean, // will be set to true if the user account  is active
-      default: true,
+      // default: true, //working on this
     },
     // this will be changed to date
     last_logged_in: {},
