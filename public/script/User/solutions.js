28a88ec69d900592e647563e35121debb6e5c572
callistus406 <callistus406@gmail.com>
2022-12-11 15:00:58 +0100

    UI update from emmanuel

---
+++ public/script/User/solutions.js
@@ -0,0 +1,28 @@
+const selectHamburger = document.querySelector('.solution-ham');
+const solutionsCreate = document.querySelector('.solutions-create');
+
+selectHamburger.addEventListener('click', function () {
+  solutionsCreate.classList.toggle('display-links');
+});
+
+// ? INTRO AND SOLUTIONS JS:
+const user = document.querySelector('.user');
+const userDetails = document.querySelector('.user-details');
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+const links = document.querySelectorAll('nav ul li');
+const bell = document.querySelector('.fa-bell');
+
+hamburger.addEventListener('click', function () {
+  nav.classList.toggle('display-nav');
+  bell.classList.toggle('display-links');
+  links.forEach(function (link) {
+    link.classList.toggle('display-links');
+  });
+  user.classList.toggle('display-links');
+  hamburger.classList.toggle('active-ham');
+});
+
+user.addEventListener('click', function () {
+  userDetails.classList.toggle('load');
+});
